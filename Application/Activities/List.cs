fa2c94ad06cddc7c67c5c4d56f3976ad42a72c22
TheDelbane <gw2enabled@gmail.com>
2023-02-18 17:34:45 +0100

    End of Section 4

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -12,33 +12,15 @@ namespace Application.Activities
 
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
-        private readonly DataContext _context;
-        private readonly ILogger<List> _logger;
-         public Handler(DataContext context, ILogger<List> logger)
+         private readonly DataContext _context;        
+         public Handler(DataContext context)
          {
-            _logger = logger;
-            _context = context;
-            
-            
+             _context = context;          
          }     
                    
             public async Task<List<Activity>> Handle(Query request, CancellationToken 
-            cancellationToken)
-            {
-                try
-                {
-                    for (var i=0; i<10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed");
-                    }
-                }
-                catch (System.Exception)
-                {
-                    
-                    _logger.LogInformation("Task was cancelled");
-                }
+            token)
+            {           
 
                 return await _context.Activities.ToListAsync();
             }
